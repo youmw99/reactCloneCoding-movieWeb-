51f123095fce516fc415cc40ae43ab0c05f31d22
Yu Minwoo <youmw99@gmail.com>
2020-07-26 22:39:28 +0900

    nomad 2.0 what is jsx and component

---
+++ src/Potato.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Potato()
+{
+  return <h3>I love Potato</h3>;
+}
+
+export default Potato;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+//render는 1개의 컴포넌트만을 rendering 해야 한다. error 발생
+//import Potato from "./Potato"
 
-
+//component : HTML을 반환하는 함수
+//jsx : angular, vue에서는 사용하지 않는 javascript 개념(?)
 ReactDOM.render(
   <React.StrictMode>
     <App />
