9ebc19ba153dc910bad68f162a6d441aa8016e89
Yu Minwoo <youmw99@gmail.com>
2020-07-27 23:09:10 +0900

    nomad coder 2.4 use react map

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function Food({name, image}){
       <h2>Potato {name}</h2>
       <img src={image}/>
   </div>
-
 }
   //jsx를 통한 argument 생성
  /* 다양한 형태의 props
@@ -23,26 +22,35 @@ function Food({name, image}){
 
 const foodILike = [
 {
+id : 1,
 name: "Kimchi",
 image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
 },
 {
+id : 2,
 name: "bulgogi",
 image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM"
 },
 {
+id : 3,
 name: "kimbap",
 image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM"
 },
 {
+id : 4,
 name: "samgyetang",
 image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
 }];
 
+function renderFood(dish){
+  return <Food key = {dish.id} name = {dish.name} image = {dish.image}/>
+}
+
 function App() {
   return (
     <div className="App">
-      {foodILike.map(dish => (<Food name = {dish.name} image = {dish.image}/>))}
+      {console.log(foodILike.map(renderFood))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
