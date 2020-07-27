a07669448d065c83ea57deda9b223e79e7b51304
Yu Minwoo <youmw99@gmail.com>
2020-07-27 22:16:11 +0900

    nomad coder 2.1 jsx + props how we can use child with arguments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import Potato from "./Potato";
+//import Potato from "./Potato";
+
+function Food({fav}){
+  //console.log(props);
+  return <h1>Potato {fav}</h1>
+}
 
 function App() {
+  //jsx를 통한 argument 생성
+ /* 다양한 형태의 props
   return (
     <div className="App">
       <h1>Hello~!</h1>
-      <Potato />
+      <Food fav="kimchi"
+        something={true}
+        papapapa={["hello",1,2,3,4,true]}
+         />
+    </div>
+  );*/
+  return (
+    <div className="App">
+      <h1>Hello~!</h1>
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="bulgogi"/>
+      <Food fav="sushi"/>
     </div>
   );
 }
