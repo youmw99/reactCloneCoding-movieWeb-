4979162c2c5ffdd22e4523f09c6aa05530abe91e
Yu Minwoo <youmw99@gmail.com>
2020-12-31 22:46:59 +0900

    Clone Coding updated

--- src/App.js
+++ src/App.js
@@ -58,12 +58,24 @@ function renderFood(dish){
   return <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating}/>
 }
 
-function App() {
-  return (
-    <div className="App">
-      {foodILike.map(renderFood)}
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    count : 0,
+
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render(){
+    return <div>
+            <h1>The number is {this.state.count}</h1>
+            <button onClick={this.add}>Add</button>
+            <button onClick={this.minus}>Minus</button>
+          </div>;
+  }
 }
 
 export default App;
